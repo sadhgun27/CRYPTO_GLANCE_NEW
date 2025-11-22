71353b2fa9c3a01e39c9c151b5bcef0546df8482
sadhgun27 <23bec014@iiitdwd.ac.in>
2025-11-22 21:48:14 +0530

    Uploaded "Coin.css, Coin.jsx"

---
+++ src/coin.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import "./Coin.css";
+
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+          <p className="coin-volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Add PropTypes validation for all props
+Coin.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  symbol: PropTypes.string.isRequired,
+  marketcap: PropTypes.number.isRequired,
+  volume: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  priceChange: PropTypes.number.isRequired
+};
+
+export default Coin;
